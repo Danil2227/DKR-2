b8d22c528ecdf6c720e0ebcfbf92a485c1db830c
Danil2227 <144226545+Danil2227@users.noreply.github.com>
2023-12-18 13:20:45 +0300

    Add files via upload

---
+++ 1 zadanie.pas
@@ -0,0 +1,29 @@
+﻿Program zad1;
+var A:array[1..10] of Integer; i: Integer;h:boolean;
+begin
+  write('Массив = ');
+  for i:=1 to 10 do
+    begin
+    A[i]:= random(1,5);
+    write(A[i]:2);
+    end;
+    writeln;
+  
+  h := true;
+  for i := 1 to 9 do
+  begin
+    if i mod 2 = 0 then
+    begin
+      if A[i] > A[i+2] then
+      begin
+      h := false;
+      Break;
+      end;
+    end;
+  end;
+  
+  if h = True then
+    Writeln('Массив является монотонным')
+  else
+    Writeln('Массив не является монотонным');
+end.
